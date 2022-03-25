faa764a11cd4000058fd19e37d7e7851e15301a4
Nikita Barskov <nbarskov@gmail.com>
2022-03-25 23:45:40 +0100

    Import all repos from GitHub.

--- terraform/github.tf
+++ terraform/github.tf
@@ -11,3 +11,28 @@ resource "github_repository" "dev" {
     "github",
   ]
 }
+
+resource "github_repository" "degree" {
+  name = "degree"
+  description = "Materials, reports, bureaucratic documents relating to my scientific work (master's thesis)."
+  visibility = "public"
+  archived = true
+}
+
+resource "github_repository" "dotfiles" {
+  name = "dotfiles"
+  description = "My dev machine configuration via ansible."
+  visibility = "public"
+  topics = [
+    "ansible",
+    "automation",
+    "dotfiles",
+    "dotfiles-ubuntu",
+    "dotfiles-macos",
+  ]
+}
+
+resource "github_repository" "sandbox_coopnorge" {
+  name = "sandbox-coopnorge"
+  visibility = "private"
+}
