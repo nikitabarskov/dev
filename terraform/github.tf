ab8eaa8d25ca7211a657c45d45caa877ad8efd1b
Nikita Barskov <nbarskov@gmail.com>
2022-03-25 23:26:07 +0100

    Make the repo public. (#4)

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,3 +1,13 @@
 resource "github_repository" "dev" {
-  name = "dev"
+  name                   = "dev"
+  visibility             = "public"
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_squash_merge     = true
+  delete_branch_on_merge = true
+  topics = [
+    "infrastructure",
+    "terraform",
+    "github",
+  ]
 }
