1858cac49acea46b7690803aab7ec2d7334fddd7
Nikita Barskov <nbarskov@gmail.com>
2022-03-26 00:37:19 +0100

    Fix an issue with GitHub OAuth token. (#8)

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,15 +3,23 @@ resource "tfe_organization" "nikita_barskov" {
   email = "nbarskov@protonmail.com"
 }
 
+resource "tfe_oauth_client" "github" {
+  name             = "terraform-github-oauth-token"
+  organization     = "nikita-barskov"
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.github_oauth_token
+  service_provider = "github"
+}
+
 resource "tfe_workspace" "dev" {
   name              = "dev"
   organization      = tfe_organization.nikita_barskov.name
   working_directory = "/terraform"
   queue_all_runs    = false
-  terraform_version = "~> 1.1"
   vcs_repo {
     identifier         = github_repository.dev.full_name
-    oauth_token_id     = var.github_oauth_token
+    oauth_token_id     = tfe_oauth_client.github.oauth_token_id
     ingress_submodules = false
   }
 }
