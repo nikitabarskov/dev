9363725998864d773144eef126b6cb4629855b67
Nikita Barskov <nbarskov@gmail.com>
2022-03-26 00:19:38 +0100

    Add terraform version.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,10 +7,11 @@ resource "tfe_workspace" "dev" {
   name              = "dev"
   organization      = tfe_organization.nikita_barskov.name
   working_directory = "/terraform"
+  queue_all_runs    = false
+  terraform_version = "~> 1.1"
   vcs_repo {
     identifier         = github_repository.dev.full_name
     oauth_token_id     = var.github_oauth_token
-    branch             = "main"
-    ingress_submodules = true
+    ingress_submodules = false
   }
 }
