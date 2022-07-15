1f9bfb9b9f3f5cc7d1554d112cf2409eaa0742f4
Nikita Barskov <nbarskov@gmail.com>
2022-07-15 22:29:57 +0200

    Add initial version of a personal website.

---
+++ blog/pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    const meta = {
+      title: 'Hello, I am Nikita',
+      description: 'My personal blog and portfolio.',
+    }
+
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
