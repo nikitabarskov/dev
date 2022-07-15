f1c96fda106ab8b15137ee9b4cfe1e79d10b3f8a
Nikita Barskov <nbarskov@gmail.com>
2022-07-15 22:50:01 +0200

    Add initial version of a personal website. (#38)
    Use https://github.com/vercel/nextjs-portfolio-starter to build my personal website.

---
+++ blog/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  unstable_staticImage: true
+})
+
+const nextConfig = {
+}
+
+module.exports = withNextra(nextConfig)
