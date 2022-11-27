af831a7f3db3bb212994d645cee15ba81a57025f
Nikita Barskov <nbarskov@gmail.com>
2022-11-27 15:11:24 +0100

    Add Analytics to blog (#224)

--- blog/pages/_app.tsx
+++ blog/pages/_app.tsx
@@ -2,6 +2,7 @@ import "nextra-theme-blog/style.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/main.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <title key="title"></title>
       </Head>
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
