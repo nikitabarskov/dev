00ffbf6937232a022b57b576afd89f004f0d4cd0
Nikita Barskov <nbarskov@gmail.com>
2022-11-27 13:49:48 +0100

    Add groups

---
+++ infrastructure/terraform/gitlab.tf
@@ -0,0 +1,13 @@
+locals {
+  groups = toset([
+    "soawesometeam",
+    "barskov-public",
+    "barskov-internal",
+  ])
+}
+
+resource "gitlab_group" "groups" {
+  for_each = local.groups
+  name        = each.value
+  path = each.value
+}
