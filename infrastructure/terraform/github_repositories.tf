e9328cc01bd35e2c33874b11413bf5039129230f
Nikita Barskov <nbarskov@gmail.com>
2022-11-27 12:43:29 +0100

    Add `homepage_url`

--- infrastructure/terraform/github_repositories.tf
+++ infrastructure/terraform/github_repositories.tf
@@ -1,5 +1,6 @@
 resource "github_repository" "dev" {
-  name = "dev"
+  name         = "dev"
+  homepage_url = "https://dev-tau-seven.vercel.app/"
 
   allow_auto_merge   = true
   allow_merge_commit = false
