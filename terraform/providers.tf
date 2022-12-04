91b7a4a407954ea383583300d1a1d63c40f7e92e
Nikita Barskov <nbarskov@gmail.com>
2022-12-04 15:09:03 +0100

    Remove old `terraform` folder (#241)
    * Remove old `terraform` folder
    
    * Update Taskfile

--- terraform/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "github" {
-  owner = "nikitabarskov"
-  token = var.github_oauth_token
-}
-
-provider "tfe" {
-  token = var.tfe_token
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "github_oauth_token" {
-  type      = string
-  sensitive = true
-}
-
-variable "tfe_token" {
-  type      = string
-  sensitive = true
-}
