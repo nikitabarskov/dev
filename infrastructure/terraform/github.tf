b3d2eb8b28a518be543ba4e304896f659850ccd5
Nikita Barskov <nbarskov@gmail.com>
2023-01-15 13:18:01 +0100

    Update deployment issues for the private repository

--- infrastructure/terraform/github.tf
+++ infrastructure/terraform/github.tf
@@ -33,36 +33,13 @@ resource "github_repository" "dev" {
 locals {
   github_repositories = {
     "data-engineer-challenge" = {
-      name       = "data-engineer-challenge"
-      archived   = true
-      visibility = "public"
-      security_and_analysis = {
-        advanced_security = {
-          status = "disabled"
-        }
-        secret_scanning = {
-          status = "disabled"
-        }
-        secret_scanning_push_protection = {
-          status = "disabled"
-        }
-      }
+      archived             = true
+      visibility           = "public"
       vulnerability_alerts = false
-    },
+    }
     "data-engineer-assignment-kaggle-january-2023" = {
-      archived   = false
-      visibility = "private"
-      security_and_analysis = {
-        advanced_security = {
-          status = "disabled"
-        }
-        secret_scanning = {
-          status = "disabled"
-        }
-        secret_scanning_push_protection = {
-          status = "disabled"
-        }
-      }
+      archived             = false
+      visibility           = "private"
       vulnerability_alerts = false
     }
   }
@@ -75,18 +52,6 @@ resource "github_repository" "private" {
   visibility = each.value.visibility
 
   archived = each.value.archived
-
-  security_and_analysis {
-    advanced_security {
-      status = each.value.security_and_analysis.advanced_security.status
-    }
-    secret_scanning {
-      status = each.value.security_and_analysis.secret_scanning.status
-    }
-    secret_scanning_push_protection {
-      status = each.value.security_and_analysis.secret_scanning_push_protection.status
-    }
-  }
 
   vulnerability_alerts = true
 
