ce1ff95f62adfa72fb577818256ac504e75e34e6
Nikita Barskov <nbarskov@gmail.com>
2023-01-31 20:16:36 +0100

    Add  to fix an issue with disabled API of Security checks in CI

--- infrastructure/terraform/github.tf
+++ infrastructure/terraform/github.tf
@@ -36,34 +36,10 @@ locals {
       name       = "data-engineer-challenge"
       archived   = true
       visibility = "public"
-      security_and_analysis = {
-        advanced_security = {
-          status = "disabled"
-        }
-        secret_scanning = {
-          status = "disabled"
-        }
-        secret_scanning_push_protection = {
-          status = "disabled"
-        }
-      }
-      vulnerability_alerts = false
     },
     "data-engineer-assignment-kaggle-january-2023" = {
       archived   = false
       visibility = "private"
-      security_and_analysis = {
-        advanced_security = {
-          status = "disabled"
-        }
-        secret_scanning = {
-          status = "disabled"
-        }
-        secret_scanning_push_protection = {
-          status = "disabled"
-        }
-      }
-      vulnerability_alerts = false
     }
   }
 }
@@ -76,18 +52,10 @@ resource "github_repository" "private" {
 
   archived = each.value.archived
 
-  security_and_analysis {
-    advanced_security {
-      status = each.value.security_and_analysis.advanced_security.status
-    }
-    secret_scanning {
-      status = each.value.security_and_analysis.secret_scanning.status
-    }
-    secret_scanning_push_protection {
-      status = each.value.security_and_analysis.secret_scanning_push_protection.status
-    }
+  lifecycle {
+    ignore_changes = [
+      security_and_analysis,
+      vulnerability_alerts,
+    ]
   }
-
-  vulnerability_alerts = true
-
 }
