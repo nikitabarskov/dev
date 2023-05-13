27ed911da6b58f8d03fc17b94eaa837c7867a2c9
Nikita Barskov <nbarskov@gmail.com>
2023-05-14 00:53:30 +0200

    Remove `tfe_oauth_client` and `dev` Terraform Cloud workspace

--- infrastructure/terraform/terraform_enterprise.tf
+++ infrastructure/terraform/terraform_enterprise.tf
@@ -8,25 +8,3 @@ resource "tfe_organization" "main" {
   name  = local.tfe.organisation_name
   email = "nbarskov@gmail.com"
 }
-
-resource "tfe_oauth_client" "github" {
-  name             = "github"
-  organization     = local.tfe.organisation_name
-  api_url          = "https://api.github.com"
-  http_url         = "https://github.com"
-  oauth_token      = var.github_oauth_token
-  service_provider = "github"
-}
-
-resource "tfe_workspace" "dev" {
-  name              = "dev"
-  organization      = tfe_organization.main.name
-  working_directory = "/infrastructure/terraform"
-  queue_all_runs    = false
-  force_delete      = true
-  vcs_repo {
-    identifier         = github_repository.public["dev"].full_name
-    oauth_token_id     = tfe_oauth_client.github.oauth_token_id
-    ingress_submodules = false
-  }
-}
