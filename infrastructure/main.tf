8c9038fad77444c33a9d5dfae647b6a0234fbf62
Nikita Barskov <nbarskov@gmail.com>
2023-05-14 00:22:29 +0200

    Fix usage of variables

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,26 @@ terraform {
   }
 }
 
+variable "github_oauth_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "gitlab_oauth_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "vercel_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "tfe_token" {
+  type      = string
+  sensitive = true
+}
+
 resource "spacelift_stack" "dev" {
   autodeploy        = false
   branch            = "main"
@@ -22,44 +42,27 @@ resource "spacelift_stack" "dev" {
 resource "spacelift_environment_variable" "tf_var_github_oauth_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_github_oauth_token"
-  value      = var.TF_VAR_github_oauth_token
+  value      = var.github_oauth_token
   write_only = true
 }
+
 resource "spacelift_environment_variable" "tf_var_vercel_api_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_vercel_api_token"
-  value      = var.TF_VAR_vercel_api_token
+  value      = var.vercel_api_token
   write_only = true
 }
+
 resource "spacelift_environment_variable" "tf_var_gitlab_oauth_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_gitlab_oauth_token"
-  value      = var.TF_VAR_gitlab_oauth_token
+  value      = var.gitlab_oauth_token
   write_only = true
 }
+
 resource "spacelift_environment_variable" "tf_var_tfe_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_tfe_token"
-  value      = var.TF_VAR_tfe_token
+  value      = var.tfe_token
   write_only = true
-}
-
-variable "TF_VAR_github_oauth_token" {
-  type      = string
-  sensitive = true
-}
-
-variable "TF_VAR_gitlab_oauth_token" {
-  type      = string
-  sensitive = true
-}
-
-variable "TF_VAR_vercel_api_token" {
-  type      = string
-  sensitive = true
-}
-
-variable "TF_VAR_tfe_token" {
-  type      = string
-  sensitive = true
 }
