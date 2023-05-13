d07a397e9eb837a67352a518737425bd877a562b
Nikita Barskov <nbarskov@gmail.com>
2023-05-14 00:25:18 +0200

    Remove `import_state_file` and pass the variables values (#451)
    * Pass the values of the variables
    
    * Add `variable`s
    
    * Use `TF_VAR_` prefix
    
    * Fix usage of variables
    
    * Enable local preview for the Spacelift stack

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,26 @@ terraform {
   }
 }
 
+variable "github_oauth_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "gitlab_oauth_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "vercel_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "tfe_token" {
+  type      = string
+  sensitive = true
+}
+
 resource "spacelift_stack" "dev" {
   autodeploy        = false
   branch            = "main"
@@ -17,29 +37,33 @@ resource "spacelift_stack" "dev" {
   project_root      = "/infrastructure/terraform"
   repository        = "dev"
   terraform_version = "1.3.6"
+  enable_local_preview = true
 }
 
 resource "spacelift_environment_variable" "tf_var_github_oauth_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_github_oauth_token"
-  value      = ""
+  value      = var.github_oauth_token
   write_only = true
 }
+
 resource "spacelift_environment_variable" "tf_var_vercel_api_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_vercel_api_token"
-  value      = ""
+  value      = var.vercel_api_token
   write_only = true
 }
+
 resource "spacelift_environment_variable" "tf_var_gitlab_oauth_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_gitlab_oauth_token"
-  value      = ""
+  value      = var.gitlab_oauth_token
   write_only = true
 }
+
 resource "spacelift_environment_variable" "tf_var_tfe_token" {
   stack_id   = spacelift_stack.dev.id
   name       = "TF_VAR_tfe_token"
-  value      = ""
+  value      = var.tfe_token
   write_only = true
 }
