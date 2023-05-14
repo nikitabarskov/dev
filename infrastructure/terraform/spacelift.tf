68aeed68924fc1ac30fedcbe2c5d1f7f22cb985e
Nikita Barskov <nbarskov@gmail.com>
2023-05-14 15:22:49 +0200

    Add `dev` stack, so Spacelift can manage Spacelift

---
+++ infrastructure/terraform/spacelift.tf
@@ -0,0 +1,27 @@
+locals {
+  stacks = {
+    "dev" = {
+      repository     = "dev"
+      administrative = true
+      autodeploy     = true
+      description    = "stack"
+      project_root   = "infrastructure/terraform"
+    }
+  }
+}
+
+resource "spacelift_stack" "stacks" {
+  for_each = local.stacks
+
+  branch     = "main"
+  name       = each.key
+  repository = each.value.repository
+
+  administrative       = each.value.administrative
+  autodeploy           = each.value.autodeploy
+  enable_local_preview = true
+  github_enterprise {
+    namespace = "nikitabarskov"
+  }
+  project_root = each.value.project_root
+}
