b188fddc34aa2a622a43c4da9f0a0069974f302d
Nikita Barskov <nbarskov@gmail.com>
2023-05-21 13:20:17 +0000

    Add `experience` repository (#477)

--- infrastructure/terraform/vercel.tf
+++ infrastructure/terraform/vercel.tf
@@ -10,3 +10,26 @@ resource "vercel_project" "dev" {
 
   root_directory = "blog"
 }
+
+locals {
+  vercel_projects = {
+    "experience" = {
+      github_repository_name = "experience"
+    }
+  }
+}
+
+resource "vercel_project" "main" {
+  for_each = local.vercel_projects
+
+  name      = each.key
+  framework = "nextjs"
+
+  git_repository = {
+    repo              = github_repository.public[each.value.github_repository_name].full_name
+    type              = "github"
+    production_branch = "main"
+  }
+
+  root_directory = "/"
+}
