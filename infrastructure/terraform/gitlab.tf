b26de91022038e919558feb1d902f3407d98ce19
Nikita Barskov <nbarskov@gmail.com>
2023-06-28 22:08:44 +0200

    Import GitLab projects branch protection (#536)

--- infrastructure/terraform/gitlab.tf
+++ infrastructure/terraform/gitlab.tf
@@ -24,3 +24,27 @@ resource "gitlab_project" "projects" {
   for_each = local.projects
   name     = each.value
 }
+
+import {
+  to = gitlab_branch_protection.main["dev"]
+  id = "25612642:master"
+}
+
+import {
+  to = gitlab_branch_protection.main["infra"]
+  id = "25780290:master"
+}
+
+import {
+  to = gitlab_branch_protection.main["research"]
+  id = "20944318:master"
+}
+
+resource "gitlab_branch_protection" "main" {
+  for_each               = local.projects
+  branch                 = "master"
+  project                = gitlab_project.projects[each.value].id
+  push_access_level      = "maintainer"
+  merge_access_level     = "maintainer"
+  unprotect_access_level = "maintainer"
+}
