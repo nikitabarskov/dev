3fa3ffbfa7a7601218422e4bb5e9d0bd82c91a38
Nikita Barskov <nbarskov@gmail.com>
2025-02-07 02:02:05 +0100

    Drop Vercel Project for my personal blog

--- infrastructure/terraform/vercel.tf
+++ infrastructure/terraform/vercel.tf
@@ -1,16 +1,3 @@
-resource "vercel_project" "dev" {
-  name      = "dev"
-  framework = "nextjs"
-
-  git_repository = {
-    repo              = github_repository.public["dev"].full_name
-    type              = "github"
-    production_branch = "main"
-  }
-
-  root_directory = "blog"
-}
-
 locals {
   vercel_projects = {
     "experience" = {
